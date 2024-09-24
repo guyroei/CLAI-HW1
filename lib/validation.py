966672a0de66507fdc968f71f70896e7c6eb7dfd
guyroei <guyroei8@gmail.com>
2024-09-24 19:15:25 +0300

    Added validation_run_2 for part 2

--- lib/validation.py
+++ lib/validation.py
@@ -57,3 +57,59 @@ def validation_run(env, net, Actions, episodes=100, device="cpu", epsilon=0.02, 
         stats['episode_steps'].append(episode_steps)
 
     return {key: np.mean(vals) for key, vals in stats.items()}
+
+def validation_run_2(env, net, ExtendedActions, episodes=100, device="cpu", epsilon=0.02, commission=0.1):
+    stats = {
+        'episode_reward': [],
+        'episode_steps': [],
+        'order_profits': [],
+        'order_steps': [],
+    }
+    extendedActionsInstance = ExtendedActions()
+    for episode in range(episodes):
+        obs, _ = env.reset()
+
+        total_reward = 0.0
+        position = None
+        position_steps = None
+        episode_steps = 0
+
+        while True:
+            obs_v = torch.tensor(np.array([obs])).to(device)
+            out_v = net(obs_v)
+
+            action_idx = out_v.max(dim=1)[1].item()
+            if np.random.random() < epsilon:
+                action_idx = env.action_space.sample()
+            action = extendedActionsInstance.actionIdxToAction(action_idx)
+
+            close_price = env._state._cur_close()
+
+            if action[0] > 0:
+                position = close_price
+                position_steps = 0
+            elif action[1] > 0:
+                profit = close_price - position - (close_price + position) * commission / 100
+                profit = 100.0 * profit / position
+                stats['order_profits'].append(profit)
+                stats['order_steps'].append(position_steps)
+                position = None
+                position_steps = None
+
+            obs, reward, done, truncated, _ = env.step(action_idx)
+            total_reward += reward
+            episode_steps += 1
+            if position_steps is not None:
+                position_steps += 1
+            if done:
+                if position is not None:
+                    profit = close_price - position - (close_price + position) * commission / 100
+                    profit = 100.0 * profit / position
+                    stats['order_profits'].append(profit)
+                    stats['order_steps'].append(position_steps)
+                break
+
+        stats['episode_reward'].append(total_reward)
+        stats['episode_steps'].append(episode_steps)
+
+    return {key: np.mean(vals) for key, vals in stats.items()}
