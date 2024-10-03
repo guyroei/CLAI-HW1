514428a524eaa64983856711d98f3b4f62aebf0b
guyroei <guyroei8@gmail.com>
2024-10-03 16:21:15 +0300

    Added validation_run_3 for part 2

--- lib/validation.py
+++ lib/validation.py
@@ -119,3 +119,85 @@ def validation_run_2(env, net, ExtendedActions, episodes=100, device="cpu", epsi
         stats['episode_steps'].append(episode_steps)
 
     return {key: np.mean(vals) for key, vals in stats.items()}
+
+
+def validation_run_3(env, net, ExtendedActions, episodes=100, device="cpu", epsilon=0.02, commission=0.1):
+    stats = {
+        'episode_reward': [],
+        'episode_steps': [],
+        'order_profits': [],
+        'order_steps': [],
+    }
+    extendedActionsInstance = ExtendedActions()
+
+    for episode in range(episodes):
+        obs, _ = env.reset()
+
+        total_reward = 0.0
+        # List to track open positions (purchase price, num of purchased shares, num of steps held)
+        open_positions = []
+        episode_steps = 0
+
+        while True:
+            obs_v = torch.tensor(np.array([obs])).to(device)
+            out_v = net(obs_v)
+
+            action_idx = out_v.max(dim=1)[1].item()
+            if np.random.random() < epsilon:
+                action_idx = env.action_space.sample()
+            action = extendedActionsInstance.actionIdxToAction(action_idx)
+
+            close_price = env._state._cur_close()
+            money_pool = env._state.moneyPool
+            owned_shares = env._state.ownShares
+
+            num_of_shares_to_buy = action[0]
+            num_of_shares_to_sell = action[1]
+
+            if num_of_shares_to_sell > 0:
+                total_profit = 0.0
+                shares_sold = 0
+
+                i = 0
+                while (i < len(open_positions)):
+                    purchase_price, purchased_shares, steps_held = open_positions[i]
+
+                    # Sold all required shares
+                    if shares_sold >= num_of_shares_to_sell:
+                        break
+
+                    shares_to_sell = min(purchased_shares, num_of_shares_to_sell - shares_sold)
+                    profit = (close_price - purchase_price)*shares_to_sell - (close_price * shares_to_sell + purchase_price * shares_to_sell)*commission/100
+                    total_profit += profit
+                    shares_sold += shares_to_sell
+                    if(shares_to_sell == purchased_shares):
+                        open_positions.pop(i)
+                    else:
+                        open_positions[i][1] = purchased_shares-(num_of_shares_to_sell - shares_sold)
+                    i += 1
+
+                stats['order_profits'].append(100.0 * total_profit / (purchase_price * num_of_shares_to_sell))
+                stats['order_steps'].append(episode_steps)
+
+            if num_of_shares_to_buy > 0:
+                cost = close_price * num_of_shares_to_buy
+                if cost <= money_pool:
+                    open_positions.append((close_price, num_of_shares_to_buy, 0))
+
+            obs, reward, done, truncated, _ = env.step(action_idx)
+            total_reward += reward
+            episode_steps += 1
+
+            # Update steps for open positions
+            i = 0
+            while(i < len(open_positions)):
+                open_positions[i][2] += 1
+                i += 1
+
+            if done:
+                break
+
+        stats['episode_reward'].append(total_reward)
+        stats['episode_steps'].append(episode_steps)
+
+    return {key: np.mean(vals) for key, vals in stats.items()}
